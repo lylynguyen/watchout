fa5e73a831a6cc7b077126c124b09ea998640874
lylynguyen <lylynguyen812@gmail.com>
2015-12-04 15:41:01 -0800

    Setup basic attributes of game

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,23 @@
+
+var gameOptions = {
+  height: 450,
+  width: 700,
+  nEnemies: 30,
+  padding: 20
+}
+
+var gameStats = {
+  score: 0,
+  bestScore: 0,
+  collisions: 0
+}
+
+var axes = {
+  x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
+  y: d3.scale.linear().domain([0,100]).range([0, gameOptions.height])
+}
+
+var gameBoard = d3.select('.board').append('svg:svg')
+                  .attr('width', gameOptions.width)
+                  .attr('height', gameOptions.height);
 // start slingin' some d3 here.
