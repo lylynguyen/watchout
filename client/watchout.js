57856c3e37a97755439ddca0185260cde506e245
lylynguyen <lylynguyen812@gmail.com>
2015-12-04 16:53:52 -0800

    Drawing board with enemies in center of page

--- client/watchout.js
+++ client/watchout.js
@@ -20,4 +20,42 @@ var axes = {
 var gameBoard = d3.select('.board').append('svg:svg')
                   .attr('width', gameOptions.width)
                   .attr('height', gameOptions.height);
-// start slingin' some d3 here.
+
+var updateScore = function(){
+  d3.select('.current')
+    .text(gameStats.score.toString());
+}
+
+var updateBestScore = function(){
+  gameStats.bestScore = d3.max([gameStats.bestScore, gameStats.score]);
+  d3.select('.highscore')
+    .text(gameStats.bestScore.toString());
+}
+
+var spawnEnemies = function(){
+  var range = d3.range(0, gameOptions.nEnemies);
+  var enemies = range.map(function(index){
+    return {
+      id: index,
+      x: Math.random() * 100,
+      y: Math.random() * 100
+    }
+  });
+
+  var drawnEnemies = gameBoard.selectAll('circle.enemy')
+                       .data(enemies, function(d){ return d.id; });
+
+  drawnEnemies.enter()
+    .append('svg:circle')
+      .attr('class', 'enemy')
+      .attr('cx', function(enemy){ return axes.x(enemy.x);})
+      .attr('cy', function(enemy){ return axes.y(enemy.y);})
+      .attr('r', 5);
+}
+
+spawnEnemies();
+
+//draw enemies
+
+//start game
+  //draw enemies
