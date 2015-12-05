e4a724d883a0068d85624973294ffbe48f4f6a64
lylynguyen <lylynguyen812@gmail.com>
2015-12-05 11:04:31 -0800

    Implemented easing transitions and styled board more effectively

--- client/watchout.js
+++ client/watchout.js
@@ -12,14 +12,63 @@ var gameStats = {
   collisions: 0
 }
 
-var axes = {
-  x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
-  y: d3.scale.linear().domain([0,100]).range([0, gameOptions.height])
-}
-
 var gameBoard = d3.select('.board').append('svg:svg')
                   .attr('width', gameOptions.width)
                   .attr('height', gameOptions.height);
+
+//returns a array of enemy objects to draw
+var spawnEnemies = function(){
+  var positions = placeEnemies();
+
+  return gameBoard.selectAll('circle') //searches for existing circle enemies
+                  .data(positions)
+                  .enter()
+                  .append('svg:circle')
+                    .attr('class', 'enemy')
+                    .attr('cx', function(enemy){ return enemy.x;})
+                    .attr('cy', function(enemy){ return enemy.y;})
+                    .attr('r', 8);
+}
+
+var placeEnemies = function(){
+  var positions = [];
+
+  //loops from 0 to nEnemies
+  for(var i = 0; i < gameOptions.nEnemies; i++){
+    var currentEnemy = {}; //enemy object with 3 properties
+    currentEnemy.id = i;
+    currentEnemy.x = Math.random() * gameOptions.width;
+    currentEnemy.y = Math.random() * gameOptions.height;
+
+    positions.push(currentEnemy); //array of all enemies
+  }
+
+  return positions;
+}
+
+//Creates enemies
+var gameEnemies = spawnEnemies();
+
+//Takes in a list of enemies
+//Put those on the board
+//if they exist, we want to update them
+var render = function(){
+  var newPositions = placeEnemies();
+
+  return gameBoard.selectAll('circle')
+                  .data(newPositions)
+                  .transition().duration(1500)
+                  .attr('cx', function(enemy){ return enemy.x;})
+                  .attr('cy', function(enemy){ return enemy.y;});
+}
+
+var play = function(){
+  setInterval(render, 2000);
+}
+
+play();
+
+
 
 var updateScore = function(){
   d3.select('.current')
@@ -31,69 +80,3 @@ var updateBestScore = function(){
   d3.select('.highscore')
     .text(gameStats.bestScore.toString());
 }
-
-//Creates enemies
-//returns a array of enemy objects to draw
-var spawnEnemies = function(){
-  var enemies = [];
-
-  //loops from 0 to nEnemies
-  for(var i = 0; i < gameOptions.nEnemies; i++){
-    var currentEnemy = {}; //enemy object with 3 properties
-    currentEnemy.id = i;
-    currentEnemy.x = Math.random() * 100;
-    currentEnemy.y = Math.random() * 100;
-
-    enemies.push(currentEnemy); //array of all enemies
-  }
-
-  return enemies;
-}
-
-//Takes in a list of enemies
-//Put those on the board
-//if they exist, we want to update them
-var render = function(enemies){
-
-  //
-  var drawnEnemies = gameBoard.selectAll('circle.enemy') //searches for existing circle enemies
-                                    .data(enemies) //apply this data thing to our enemies
-                                    
-
-                                    // .transition().duration(1500)
-                                    // .tween('custom', function(enemyData){
-                                    //   currentDot = d3.select(this);
-                                    //   return currentDot.attr('cx', axes.x(enemyData.x)).attr('cy', axes.y(enemyData.y));
-                                    // })
-                                    .attr('cx', function(enemy){ return axes.x(enemy.x);})
-                                    .attr('cy', function(enemy){ return axes.y(enemy.y);})
-                                    .enter()
-                                      .append('svg:circle')
-                                        .attr('class', 'enemy')
-                                        .attr('cx', function(enemy){ return axes.x(enemy.x);})
-                                        .attr('cy', function(enemy){ return axes.y(enemy.y);})
-                                        .attr('r', 5);
-
-  
-
-  // drawnEnemies.exit().remove();
-    console.log("called");
-
-  return drawnEnemies;
-}
-
-var play = function(){
-  var gameTurn = function(){
-    var newEnemies = spawnEnemies();
-    return render(newEnemies);
-  }
-
-  gameTurn();
-  setInterval(gameTurn, 2000);
-}
-
-play();
-//draw enemies
-
-//start game
-  //draw enemies
