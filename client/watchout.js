2bfc51f3a7a78f6e836185b7530f1a7d57cd8ff7
lylynguyen <lylynguyen812@gmail.com>
2015-12-04 18:30:41 -0800

    Added moving nodes to our board. Working well but transitions fail

--- client/watchout.js
+++ client/watchout.js
@@ -2,7 +2,7 @@
 var gameOptions = {
   height: 450,
   width: 700,
-  nEnemies: 30,
+  nEnemies: 3, //number of enemies we want to have in the game
   padding: 20
 }
 
@@ -32,29 +32,67 @@ var updateBestScore = function(){
     .text(gameStats.bestScore.toString());
 }
 
+//Creates enemies
+//returns a array of enemy objects to draw
 var spawnEnemies = function(){
-  var range = d3.range(0, gameOptions.nEnemies);
-  var enemies = range.map(function(index){
-    return {
-      id: index,
-      x: Math.random() * 100,
-      y: Math.random() * 100
-    }
-  });
+  var enemies = [];
 
-  var drawnEnemies = gameBoard.selectAll('circle.enemy')
-                       .data(enemies, function(d){ return d.id; });
+  //loops from 0 to nEnemies
+  for(var i = 0; i < gameOptions.nEnemies; i++){
+    var currentEnemy = {}; //enemy object with 3 properties
+    currentEnemy.id = i;
+    currentEnemy.x = Math.random() * 100;
+    currentEnemy.y = Math.random() * 100;
 
-  drawnEnemies.enter()
-    .append('svg:circle')
-      .attr('class', 'enemy')
-      .attr('cx', function(enemy){ return axes.x(enemy.x);})
-      .attr('cy', function(enemy){ return axes.y(enemy.y);})
-      .attr('r', 5);
+    enemies.push(currentEnemy); //array of all enemies
+  }
+
+  return enemies;
 }
 
-spawnEnemies();
+//Takes in a list of enemies
+//Put those on the board
+//if they exist, we want to update them
+var render = function(enemies){
 
+  //
+  var drawnEnemies = gameBoard.selectAll('circle.enemy') //searches for existing circle enemies
+                                    .data(enemies) //apply this data thing to our enemies
+                                    
+
+                                    // .transition().duration(1500)
+                                    // .tween('custom', function(enemyData){
+                                    //   currentDot = d3.select(this);
+                                    //   return currentDot.attr('cx', axes.x(enemyData.x)).attr('cy', axes.y(enemyData.y));
+                                    // })
+                                    .attr('cx', function(enemy){ return axes.x(enemy.x);})
+                                    .attr('cy', function(enemy){ return axes.y(enemy.y);})
+                                    .enter()
+                                      .append('svg:circle')
+                                        .attr('class', 'enemy')
+                                        .attr('cx', function(enemy){ return axes.x(enemy.x);})
+                                        .attr('cy', function(enemy){ return axes.y(enemy.y);})
+                                        .attr('r', 5);
+
+  
+
+  // drawnEnemies.exit().remove();
+    console.log("called");
+
+  return drawnEnemies;
+}
+
+var play = function(){
+  var gameTurn = function(){
+    var newEnemies = spawnEnemies();
+    return render(newEnemies);
+  }
+
+  gameTurn();
+  setInterval(gameTurn, 2000);
+}
+
+play();
 //draw enemies
 
 //start game
